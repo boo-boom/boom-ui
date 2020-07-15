13420783a5e35975f9184a72b1b2b22632bba45c
czx <zhixiang.chen@xkcun.com>
2020-07-15 09:52:15 +0800

    init

---
+++ jest.test.js
@@ -0,0 +1,4 @@
+test('测试2+2', () => {
+  expect(2 + 2).toBe(4)
+  expect(2 + 2).not.toBe(5)
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button autoFocus btnType={ButtonType.Default} size={ButtonSize.Large}>Large Default</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>Large Danger</Button>
+      <Button btnType={ButtonType.Default} size={ButtonSize.Small}>Small Default</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Small Primary</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
+      <Button btnType={ButtonType.Default} size={ButtonSize.Small} disabled>Disabled Default</Button>
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com">Link</Button>
+      <Button btnType={ButtonType.Link} disabled href="https://www.baidu.com">Link</Button>
     </div>
   );
 }
