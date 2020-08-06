17e8d0f6b15ba4be4a9de930a29322ddf2fdba96
czx <zhixiang.chen@xkcun.com>
2020-08-06 12:05:12 +0800

    travis-ci自动生成文档

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -7,6 +7,7 @@ storiesOf('Welcome page', module).add(
     return (
       <>
         <h1>欢迎来到 boom-component 组件库</h1>
+        <img src="https://travis-ci.com/boo-boom/boom-ui.svg?branch=master" alt="travis"/>
         <p>boom-component，从零搭建组件库</p>
         <h3>安装试试</h3>
         <code>npm install boom-component --save</code>
